26059773982f1bb0fdf0a16ad8c61e462ad4ec29
serinaqin <qin.zhen@northeastern.edu>
2024-09-19 22:11:59 -0700

    initialize be

---
+++ pawfectmatch-be/src/main/java/com/serina/pawfectmatchbe/PawfectmatchBeApplication.java
@@ -0,0 +1,13 @@
+package com.serina.pawfectmatchbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawfectmatchBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawfectmatchBeApplication.class, args);
+    }
+
+}
---
+++ pawfectmatch-be/src/test/java/com/serina/pawfectmatchbe/PawfectmatchBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serina.pawfectmatchbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawfectmatchBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
